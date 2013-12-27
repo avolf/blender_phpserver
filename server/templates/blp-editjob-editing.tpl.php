053f9f4b40b2dc1b0112d3b1473c9b536931613b
avolf <alex@alexandervolf.com>
2013-12-27 01:17:12 +0100

    newjob/editjob: fixed template form.

--- server/templates/blp-editjob-editing.tpl.php
+++ server/templates/blp-editjob-editing.tpl.php
@@ -5,9 +5,11 @@ $job->read();
 ?>
 Editing job <?php echo $job->getName() ?>:
 <form action="editjob.php?done=1&id=<?php echo $jobid; ?>" method="post">
- <p>Job Name: <input type="text" name="name" value="<?php echo $job->getName(); ?>"/></p>
- <p>Start Frame: <input type="text" name="start" value="<?php echo $job->getBegin(); ?>"/></p>
- <p>End Frame: <input type="text" name="end" value="<?php echo $job->getEnd(); ?>" /></p>
- <p><input type="submit" /></p>
+<p><table>
+<tr><td>Job Name:</td><td><input type="text" name="name" value="<?php echo $job->getName(); ?>"/></td></tr>
+<tr><td>Start Frame:</td><td><input type="text" name="start" value="<?php echo $job->getBegin(); ?>"/></td></tr>
+<tr><td>End Frame:</td><td><input type="text" name="end" value="<?php echo $job->getEnd(); ?>"/></td></tr>
+</table></p>
+<p><input type="submit" /></p>
 </form>
 <a class="button" href="deljob.php?id=<?php echo $jobid; ?>">Delete</a>
--- server/templates/blp-newjob.tpl.php
+++ server/templates/blp-newjob.tpl.php
@@ -1,7 +1,9 @@
 Create new job:
 <form action="newjob.php?done=1" method="post">
- <p>Job Name: <input type="text" name="name" /></p>
- <p>Start Frame: <input type="text" name="start" /></p>
- <p>End Frame: <input type="text" name="end" /></p>
- <p><input type="submit" /></p>
+<p><table>
+<tr><td>Job Name:</td><td><input type="text" name="name" /></td></tr>
+<tr><td>Start Frame:</td><td><input type="text" name="start" /></td></tr>
+<tr><td>End Frame:</td><td><input type="text" name="end" /></td></tr>
+</table></p>
+<p><input type="submit" /></p>
 </form>
